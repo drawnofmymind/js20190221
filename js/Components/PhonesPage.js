68c1af42d6d6a4db7bd789f51f01ce0dc54ebe97
vitaly <kitkat978@mail.ru>
2019-04-07 20:00:37 +0700

    Phones are now rendered from array

--- js/Components/PhonesPage.js
+++ js/Components/PhonesPage.js
@@ -2,33 +2,43 @@ import Filter from "./Filter.js";
 import PhonesCatalog from "./PhonesCatalog.js";
 import ShoppingCart from "./ShoppingCart.js";
 
+import {getAll,getById} from '../api/phones.js';
+
 export default class PhonesPage {
     constructor(element) {
      this.element = element; 
      this.render();
-     new PhonesCatalog(this.element.querySelector('[data-component="PhonesCatalog"]'))
-     new Filter(this.element.querySelector('[data-component="Filter"]'))
-     new ShoppingCart(this.element.querySelector('[data-component="ShoppingCart"]'))
+
+     this.initComponent(PhonesCatalog,{
+       phones: getAll()
+     });
+     this.initComponent(Filter);
+     this.initComponent(ShoppingCart)
   }
-    render() {
-     this.element.innerHTML = `
-     <div class="row">
-  
-     <!--Sidebar-->
-     <div class="col-md-2">
-       <section>
-        <div data-component = "Filter"></div>
-       </section>
-  
-       <section>
-       <div data-component = "ShoppingCart"></div>
-       </section>
-     </div>
-  
-     <!--Main content-->
-     <div class="col-md-10">
-     <div data-component = "PhonesCatalog"></div>
-     </div>
-  `;
-    }
+  initComponent(Constructor,props = {}){
+    new Constructor(this.element.querySelector(`[data-component="${Constructor.name}"]`),
+    props
+    );
   }
+  render() {
+  this.element.innerHTML = `
+  <div class="row">
+
+  <!--Sidebar-->
+  <div class="col-md-2">
+    <section>
+      <div data-component = "Filter"></div>
+    </section>
+
+    <section>
+    <div data-component = "ShoppingCart"></div>
+    </section>
+  </div>
+
+  <!--Main content-->
+  <div class="col-md-10">
+  <div data-component = "PhonesCatalog"></div>
+  </div>
+`;
+  }
+}
