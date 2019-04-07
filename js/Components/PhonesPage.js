658429447abc4fcb82f2e6d4969db6ead1e526cd
vitaly <kitkat978@mail.ru>
2019-04-07 14:23:23 +0700

    components was added

---
+++ js/Components/PhonesPage.js
@@ -0,0 +1,34 @@
+import Filter from "./Filter.js"
+import PhonesCatalog from "./PhonesCatalog.js"
+import ShoppingCart from "./ShoppingCart"
+
+export default class PhonesPage {
+    constructor(element) {
+     this.element = element; 
+     this.render();
+     new PhonesCatalog(this.element.querySelector('[data-component="PhonesCatalog"]'))
+     new Filter(this.element.querySelector('[data-component="Filter"]'))
+     new ShoppingCart(this.element.querySelector('[data-component="ShoppingCart"]'))
+  }
+    render() {
+     this.element.innerHTML = `
+     <div class="row">
+  
+     <!--Sidebar-->
+     <div class="col-md-2">
+       <section>
+        <div data-component = "Filter"></div>
+       </section>
+  
+       <section>
+       <div data-component = "ShoppingCart"></div>
+       </section>
+     </div>
+  
+     <!--Main content-->
+     <div class="col-md-10">
+       
+     </div>
+  `;
+    }
+  }
